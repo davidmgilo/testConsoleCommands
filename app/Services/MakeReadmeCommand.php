3f4abfffa9c7da848a63c650e8280adf2161d7e6
David Martinez <davidmgilo@gmail.com>
2017-01-31 21:41:17 +0100

    make readme II

--- app/Services/MakeReadmeCommand.php
+++ app/Services/MakeReadmeCommand.php
@@ -9,10 +9,26 @@
 namespace App\Services;
 
 
+use Illuminate\Filesystem\Filesystem;
+
 class MakeReadmeCommand
 {
+    protected $filesystem;
+
+    /**
+     * MakeReadmeCommand constructor.
+     * @param $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+
     public function create()
     {
         dump("file readme created");
+        //copy stub to /
+        $this->filesystem->copy(__DIR__ . '/stubs/MyReadme.md','../../MyReadme.md');
     }
 }
