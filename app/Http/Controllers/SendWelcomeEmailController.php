50ea4590098ccc61f7c92eb709b0e22519b38e89
David Martinez <davidmgilo@gmail.com>
2017-01-31 21:13:05 +0100

    sending email III - dependency injection

--- app/Http/Controllers/SendWelcomeEmailController.php
+++ app/Http/Controllers/SendWelcomeEmailController.php
@@ -2,18 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SendwelcomeEmail;
 use Illuminate\Http\Request;
 
 class SendWelcomeEmailController extends Controller
 {
     //
+    public $email;
+
+    /**
+     * SendWelcomeEmailController constructor.
+     * @param $email
+     */
+    public function __construct(SendwelcomeEmail $email)
+    {
+        $this->email = $email;
+    }
+
+
     /**
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        dump("Sending email!");
+        $this->email->send();
 
         //$data = [];
         //return view('sendWelcomeEmail',$data);
